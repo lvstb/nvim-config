e09e4b5b30349d3d0b12e9cdedf162985b823eb6
Lars Van Steenbergen <lars@wingu.dev>
2025-06-12 11:12:36 +0200

    feat: add schemastore for yaml

--- lua/modules/plugins/completion.lua
+++ lua/modules/plugins/completion.lua
@@ -1,18 +1,9 @@
 local completion = {}
 
--- completion["neovim/nvim-lspconfig"] = {
--- 	lazy = true,
--- 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
--- 	config = require("completion.lsp"),
--- 	dependencies = {
--- 		{ "williamboman/mason.nvim" },
--- 		{ "folke/neodev.nvim" },
--- 		{ "b0o/schemastore.nvim" },
--- 		{ "williamboman/mason-lspconfig.nvim" },
--- 		{ "hrsh7th/cmp-nvim-lsp" },
--- 		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
--- 	},
--- }
+completion["b0o/schemastore.nvim"] = {
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+}
 
 completion["saghen/blink.cmp"] = {
 	dependencies = {
@@ -40,36 +31,6 @@ completion["dnlhc/glance.nvim"] = {
 	event = "LspAttach",
 	config = require("completion.glance"),
 }
--- completion["hrsh7th/nvim-cmp"] = {
--- 	lazy = true,
--- 	event = "InsertEnter",
--- 	config = require("completion.cmp"),
--- 	dependencies = {
--- 		{
--- 			"L3MON4D3/LuaSnip",
--- 			dependencies = { "rafamadriz/friendly-snippets" },
--- 			config = require("completion.luasnip"),
--- 		},
--- 		{ "lukas-reineke/cmp-under-comparator" },
--- 		{ "saadparwaiz1/cmp_luasnip" },
--- 		{ "hrsh7th/cmp-nvim-lsp" },
--- 		{ "hrsh7th/cmp-nvim-lua" },
--- 		{ "andersevenrud/cmp-tmux" },
--- 		{ "hrsh7th/cmp-path" },
--- 		{ "f3fora/cmp-spell" },
--- 		{ "hrsh7th/cmp-buffer" },
--- 		{ "kdheepak/cmp-latex-symbols" },
--- 		{ "ray-x/cmp-treesitter" },
--- 		-- {
--- 		-- 	"jcdickinson/codeium.nvim",
--- 		-- 	dependencies = {
--- 		-- 		"nvim-lua/plenary.nvim",
--- 		-- 		"MunifTanjim/nui.nvim",
--- 		-- 	},
--- 		-- 	config = require("completion.codeium"),
--- 		-- },
--- 	},
--- }
 
 completion["zbirenbaum/copilot.lua"] = {
 	lazy = true,
